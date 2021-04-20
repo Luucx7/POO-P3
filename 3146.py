7a658c62d3606b1a3e2c1730715d9a9d5daade31
Lucas <lukinhasmlopes@hotmail.com>
2021-04-20 00:13:23 -0300

    Initial commit

---
+++ 3146.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+entrada = float(input())  # Recebe o valor de entrada e converte para ponto flutuante
+circunferencia = 2 * 3.14 * entrada  # Aplica a fórmula da circunferência, considerando pi = 3.14
+print("%.2f" % circunferencia)  # Imprime o resultado com duas casar decimais
